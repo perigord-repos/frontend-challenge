0d33b321453458d1f80dd175f6c5f756be95fa99
Michael <michael.gannon13@gmail.com>
2023-10-25 20:25:58 +0100

    fixed unit tests

--- tic-tac-toe-challenge/cypress/e2e/Square.cy.js
+++ tic-tac-toe-challenge/cypress/e2e/Square.cy.js
@@ -7,32 +7,25 @@ describe('Square component', () => {
   });
 
   it('should render the game board with 9 squares', () => {
-    cy.get('.board').eq(1).should('be.visible');
-    cy.get('.board').eq(1).find('[data-testid="square"]').should('have.length', 9)
+    cy.get('.board').should('be.visible');
+    cy.get('.board').find('[data-testid="square"]').should('have.length', 9)
     
   })
 
   it('should be able to play a game of tic tac toe', () => {
-    cy.get('.board').eq(1)
-    .find('[data-testid="square"]')
-    .first()
-    .click()
-    .find('svg#x') 
-    .should('exist');  
-   
-    cy.get('.board').eq(1)
-    .find('[data-testid="square"]').eq(1)  
-    .click()
-    .find('svg#o') 
-    .should('exist'); 
-  })
+    cy.get('.board').find('[data-testid="square"]').first().click();
+    cy.get('.board').find('svg#x').should('exist');
+    cy.get('.board').find('[data-testid="square"]').eq(1).click();
+    cy.get('.board').find('svg#o').should('exist');
+  });
+  
 
   it('should be able to win a game', () => {
-    cy.get('.board').eq(1).find('[data-testid="square"]').eq(0).click();
-    cy.get('.board').eq(1).find('[data-testid="square"]').eq(3).click();
-    cy.get('.board').eq(1).find('[data-testid="square"]').eq(1).click();
-    cy.get('.board').eq(1).find('[data-testid="square"]').eq(4).click();
-    cy.get('.board').eq(1).find('[data-testid="square"]').eq(2).click();
+    cy.get('.board').find('[data-testid="square"]').eq(0).click({ force: true });
+    cy.get('.board').find('[data-testid="square"]').eq(3).click({ force: true });
+    cy.get('.board').find('[data-testid="square"]').eq(1).click({ force: true });
+    cy.get('.board').find('[data-testid="square"]').eq(4).click({ force: true });
+    cy.get('.board').find('[data-testid="square"]').eq(2).click({ force: true });
 
     cy.get('.modal-body').should('be.visible').and('contain', 'X wins!');
 })
