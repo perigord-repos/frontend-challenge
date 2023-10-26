7d8405bcf4a7f4f4189a8390a3ddc4605c719168
Michael <michael.gannon13@gmail.com>
2023-10-26 15:16:06 +0100

    changes

--- tic-tac-toe-challenge/cypress/integration/e2e/App.cy.js
+++ tic-tac-toe-challenge/cypress/integration/e2e/App.cy.js
@@ -12,7 +12,6 @@ describe('Tic Tac Toe App', () => {
     });
   
     it('renders the game board', () => {
-      // Assuming 1 Board components render 
       cy.get('.game-board').should('be.visible');
       cy.get('.game-board .board').should('have.length', 1);
     });
@@ -21,20 +20,16 @@ describe('Tic Tac Toe App', () => {
       cy.get('.players-board-container').should('have.length', 1);
       cy.get('.players-board-container .player').eq(0).should('contain', 'PLAYER 1');
       cy.get('.players-board-container .player').eq(1).should('contain', 'PLAYER 2');
-      
-      // Check wins for each player (requires initial state or stubbing data)
       cy.get('.players-board-container .player-wins').eq(0).should('contain', '0');
       cy.get('.players-board-container .player-wins').eq(1).should('contain', '0');
-    
     });
-    
-  
+
     it('renders game timer and stats', () => {
       cy.get('.game-timer').should('be.visible');
       cy.get('.stats-container').should('be.visible');
       cy.get('.right-content').should('be.visible');
     });
-  
+
   });
   
   
